f6b9912f95cf57106c8b1857c793dfdce777da4d
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2019-01-24 15:06:24 +0100

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Setup for beets-extrafiles."""
+from setuptools import setup
+
+setup(
+    name='beets-extrafiles',
+    version='0.0.1',
+    description=(
+        'A plugin for beets that copies additional files and directories '
+        'during the import process.'
+    ),
+    author='Jan Holthuis',
+    author_email='holthuis.jan@gmail.com',
+    license='MIT',
+    packages=['beetsplug'],
+    namespace_packages=['beetsplug'],
+    install_requires=['beets>=1.4.7'],
+    classifiers=[
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
