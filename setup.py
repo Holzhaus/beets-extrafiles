d1e47393eafbc737582d5e960738699145f7a3ef
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2019-01-25 10:43:53 +0100

    setup.py: Add README as long_description

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Setup for beets-extrafiles."""
+import os
+
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    readme = f.read()
 
 setup(
     name='beets-extrafiles',
@@ -10,6 +15,8 @@ setup(
         'A plugin for beets that copies additional files and directories '
         'during the import process.'
     ),
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author='Jan Holthuis',
     author_email='holthuis.jan@gmail.com',
     url='https://github.com/Holzhaus/beets-extrafiles',
